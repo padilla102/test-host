7db495f2375319cab159734f73f83449d1c2339c
Jhon Padilla <padilla102@gmail.com>
2021-09-29 17:18:32 -0500

    commit initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { loadRemoteModule } from '@angular-architects/module-federation-runtime';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      loadRemoteModule({
+        remoteEntry: 'http://localhost:4200/remoteEntry.js',
+        remoteName: 'testMicrofront',
+        exposedModule: './Module',
+      }).then((m) => m.ModuloUnoModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
